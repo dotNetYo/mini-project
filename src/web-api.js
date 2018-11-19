04b2d741515e8e9c6b5b751531a167e0787178c4
Yoel <yoel_levy_1@hotmail.fr>
2018-11-19 23:59:41 +0200

    first commit

---
+++ src/web-api.js
@@ -0,0 +1,120 @@
+import {HttpClient, json} from 'aurelia-fetch-client';
+
+let httpClient = new HttpClient();
+
+let latency =200;
+let id = 0;
+
+function getId(){
+  return ++id;
+}
+
+let contacts = [
+  {
+    id:getId(),
+    firstName:'John',
+    lastName:'Tolkien',
+    email:'tolkien@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:getId(),
+    firstName:'Clive',
+    lastName:'Lewis',
+    email:'lewis@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:getId(),
+    firstName:'Owen',
+    lastName:'Barfield',
+    email:'barfield@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:getId(),
+    firstName:'Charles',
+    lastName:'Williams',
+    email:'williams@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:getId(),
+    firstName:'Roger',
+    lastName:'Green',
+    email:'green@inklings.com',
+    phoneNumber:'867-5309'
+  }
+];
+
+export class WebAPI {
+  isRequesting = false;
+  
+  getContactList(){
+    /*
+    this.isRequesting = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        let results = contacts.map(x =>  { return {
+          id:x.id,
+          firstName:x.firstName,
+          lastName:x.lastName,
+          email:x.email
+        }});
+        resolve(results);
+        this.isRequesting = false;
+      }, latency);
+    });*/
+    this.isRequesting = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+      httpClient.fetch('http://jsonplaceholder.typicode.com/posts/')
+      .then(response => response.json())
+      .then(data => {
+        let results = data.map(x =>  { return {
+          id:x.id,
+          firstName:'',   
+          lastName:'',      
+          email:x.title
+        }});
+        resolve(results);
+        this.isRequesting = false;
+      });
+       
+      }, latency);
+    });
+  }
+
+  getContactDetails(id){
+    this.isRequesting = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        let found = contacts.filter(x => x.id == id)[0];
+        resolve(JSON.parse(JSON.stringify(found)));
+        this.isRequesting = false;
+      }, latency);
+    });
+  
+  }
+
+  saveContact(contact){
+    this.isRequesting = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        let instance = JSON.parse(JSON.stringify(contact));
+        let found = contacts.filter(x => x.id == contact.id)[0];
+
+        if(found){
+          let index = contacts.indexOf(found);
+          contacts[index] = instance;
+        }else{
+          instance.id = getId();
+          contacts.push(instance);
+        }
+
+        this.isRequesting = false;
+        resolve(instance);
+      }, latency);
+    });
+  }
+}
